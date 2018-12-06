b6b65ef1f47b192e3bf6602d3b9668320b1b14d6
linye <creator_ly@163.com>
2018-12-06 11:17:49 +0800

    add blog project and test app

---
+++ app_test/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from app_test import models
+
+# Create your views here.
+
+userlist = []
+
+def test(request):
+    # 1.return data
+    # return HttpResponse("Hello World!")
+    
+    # 2.retutn enter date
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+    #     print(username,password)
+    #     tempuser = {'user':username, 'pwd':password}
+    #     userlist.append(tempuser)
+
+    # return render(request,'test.html', {'data':userlist})
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # 将数据保存到数据库
+        models.UserInfo.objects.create(user = username, pwd = password)
+        
+    # 将数据库中的所有数据读取出来
+    userlist = models.UserInfo.objects.all()
+    return render(request,'test.html', {'data':userlist})
+
+    
